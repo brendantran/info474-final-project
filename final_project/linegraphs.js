dfc02718df7dbf3f0d8f11e3bac82c44caf5a5ff
mariamatlick <mmatlick@uw.edu>
2021-12-05 21:41:29 -0800

    draw lines for overall line chart

--- final_project/linegraphs.js
+++ final_project/linegraphs.js
@@ -5,6 +5,7 @@ var margin = { top: 10, right: 30, bottom: 150, left: 100 },
 // append the svg object to the body of the page
 var svg = d3.select("#allraces")
   .append("svg")
+  .attr("id", "overallChart")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
@@ -12,37 +13,36 @@ var svg = d3.select("#allraces")
     "translate(" + margin.left + "," + margin.top + ")")
 
 
-// OVERALL/TOP LINE CHART WITH ALL RACES/ETHNICITIES ---------------------------------------------------
-d3.csv("/csv/educational-attainment-all.csv", function (d) {
+// OVERALL/TOP LINE CHART WITH ALL RACES/ETHNICITIES AND ALL GENDERS
+d3.csv("/csv/educational-attainment-all.csv", function (dataset) {
   // format variables 
   // array of grade levels (strings)
-  var grades = d.map(d => d.grade)
+  var grades = dataset.map(dataset => dataset.grade)
   window.value = grades
-  console.log(grades)
-
   // array of numbers of asian graduates (integers)
-  var asianTotalGraduateString = d.map(d => d.asian)
+
+  var asianTotalGraduateString = dataset.map(dataset => dataset.asian)
   var asianTotalGraduates = []
   for (i = 0; i <= asianTotalGraduateString.length; i++) {
     asianTotalGraduates.push(parseInt(asianTotalGraduateString[i]))
   }
-  console.log(d)
+
   // array of numbers of black graduates (integers)
-  var blackTotalGraduateString = d.map(d => d.black)
+  var blackTotalGraduateString = dataset.map(dataset => dataset.black)
   var blackTotalGraduates = []
   for (i = 0; i <= blackTotalGraduateString.length; i++) {
     blackTotalGraduates.push(parseInt(blackTotalGraduateString[i]))
   }
 
   // array of numbers of hispanic graduates (integers)
-  var hispanicTotalGraduateString = d.map(d => d.hispanic)
+  var hispanicTotalGraduateString = dataset.map(dataset => dataset.hispanic)
   var hispanicTotalGraduates = []
   for (i = 0; i <= hispanicTotalGraduateString.length; i++) {
     hispanicTotalGraduates.push(parseInt(hispanicTotalGraduateString[i]))
   }
 
   // array of numbers of hispanic graduates (integers)
-  var whiteTotalGraduateString = d.map(d => d.white)
+  var whiteTotalGraduateString = dataset.map(dataset => dataset.white)
   var whiteTotalGraduates = []
   for (i = 0; i <= whiteTotalGraduateString.length; i++) {
     whiteTotalGraduates.push(parseInt(whiteTotalGraduateString[i]))
@@ -68,10 +68,14 @@ d3.csv("/csv/educational-attainment-all.csv", function (d) {
     .attr("y", 410)
 
   // Add Y axis
-  var yScale = d3.scaleLinear().domain([0, d3.max(asianTotalGraduates)]).range([height, 0])
+  
+  //var yScale = d3.scaleLinear().domain([0, d3.max(whiteTotalGraduates)]).range([height, 0])
+  var yScale = d3.scaleLinear().domain([0, d3.max(dataset, function(d) { 
+    return +d.white;
+  })]).range([height, 0])
   svg.append("g")
     .call(d3.axisLeft(yScale))
-
+ 
   // y-axis label
   svg.append('text')
     .text("NUMBER OF GRADUATES")
@@ -82,33 +86,75 @@ d3.csv("/csv/educational-attainment-all.csv", function (d) {
 
   // Add the line
   svg.append("path")
-    .data(d)
+    .datum(dataset)
     .attr("class", "asianLine")
     .attr("fill", "none")
-    .attr("stroke", "steelblue")
+    .attr("stroke", "#69b3a2")
     .attr("stroke-width", 1.5)
     .attr("d", d3.line()
-      .x(grades)
-      .y(function (d) { return yScale(asianTotalGraduates); })
+      .x(function (d) { return xScale(d.grade); })
+      .y(function (d) { return yScale(d.asian); })
     )
-
+    svg.append("path")
+    .datum(dataset)
+    .attr("class", "blackLine")
+    .attr("fill", "none")
+    .attr("stroke", "#FA6900")
+    .attr("stroke-width", 1.5)
+    .attr("d", d3.line()
+      .x(function (d) { return xScale(d.grade); })
+      .y(function (d) { return yScale(d.black); })
+    )
+    svg.append("path")
+    .datum(dataset)
+    .attr("class", "hispanicLine")
+    .attr("fill", "none")
+    .attr("stroke", "#187bcd")
+    .attr("stroke-width", 1.5)
+    .attr("d", d3.line()
+      .x(function (d) { return xScale(d.grade); })
+      .y(function (d) { return yScale(d.hispanic); })
+    )
+    svg.append("path")
+    .datum(dataset)
+    .attr("class", "whiteLine")
+    .attr("fill", "none")
+    .attr("stroke","#785Bd7")
+    .attr("stroke-width", 1.5)
+    .attr("d", d3.line()
+      .x(function (d) { return xScale(d.grade); })
+      .y(function (d) { return yScale(d.white); })
+    )
+    
   // chart legend
   svg.append("circle").attr("cx",700).attr("cy",130).attr("r", 6).style("fill", "#69b3a2")
-  svg.append("circle").attr("cx",700).attr("cy",160).attr("r", 6).style("fill", "#785Bd7")
+  svg.append("circle").attr("cx",700).attr("cy",160).attr("r", 6).style("fill", "#FA6900")
   svg.append("circle").attr("cx",700).attr("cy",190).attr("r", 6).style("fill", "#187bcd")
-  svg.append("circle").attr("cx",700).attr("cy",220).attr("r", 6).style("fill", "#FA6900")
+  svg.append("circle").attr("cx",700).attr("cy",220).attr("r", 6).style("fill", "#785Bd7")
   svg.append("text").attr("x", 720).attr("y", 130).text("Asian").style("font-size", "15px").attr("alignment-baseline","middle")
   svg.append("text").attr("x", 720).attr("y", 160).text("Black").style("font-size", "15px").attr("alignment-baseline","middle")
   svg.append("text").attr("x", 720).attr("y", 190).text("Hispanic").style("font-size", "15px").attr("alignment-baseline","middle")
   svg.append("text").attr("x", 720).attr("y", 220).text("White").style("font-size", "15px").attr("alignment-baseline","middle")
-  
 
+  
 })
 
+function drawAll() {
+  //console.log("drawAll WORKS when you click the male radio button")
+}
+function drawMale() {
+  //console.log("drawMale WORKS when you click the male radio button")
+  //var removeOverall = d3.select("#overallChart")
+  //removeOverall.selectAll("*").remove()
+  
+}
+function drawFemale() {
+  //console.log("drawFemale WORKS when you click the male radio button")
+}
 
 // // MINI CHARTS CODE BELOW ---------------------------------------------------
 d3.csv('/csv/educational-attainment-asian.csv', function(dataset) {
-console.log(dataset)
+//console.log(dataset)
 
   var margin = { top: 5, right: 15, bottom: 75, left: 50 },
   width = 450 - margin.left - margin.right,
@@ -121,7 +167,7 @@ console.log(dataset)
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
-
+ 
   var grades = window.value
   var xScale = d3.scalePoint().domain(grades).range([0, width]);
   var xAxis = d3.axisBottom(xScale).ticks(16)
@@ -143,7 +189,7 @@ console.log(dataset)
   svg.append('path')
     .datum(dataset)
     .attr('fill', 'none')
-    .attr('stroke', 'steelblue')
+    .attr('stroke', '#69b3a2')
     .attr('stroke-width', 1.5)
     .attr('d', d3.line()
       .x(function(d) { return d.Coordinates })
